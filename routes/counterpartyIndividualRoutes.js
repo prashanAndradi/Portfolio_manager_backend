96e174082aef0c24b58190738b324f652fe0e237
unknown <prashanandradi@gmail.com>
2026-09-08 11:50:09 +0530

    Restrict individual counterparty create and update to Middle Office.

--- routes/counterpartyIndividualRoutes.js
+++ routes/counterpartyIndividualRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const counterpartyIndividualController = require('../controllers/counterpartyIndividualController');
+const { checkAuth } = require('../middleware/auth');
+const { requireMiddleOffice } = require('../middleware/requireMiddleOffice');
 
+// Counterparty master setup (create/edit) is Middle Office-exclusive - the
+// dropdown/lookup GET routes below stay open, since every front-office user
+// needs them for deal entry.
 // POST /api/counterparty-individual
-router.post('/', counterpartyIndividualController.createCounterpartyIndividual);
+router.post('/', checkAuth, requireMiddleOffice, counterpartyIndividualController.createCounterpartyIndividual);
 
 // GET /api/counterparty-individual
 router.get('/', counterpartyIndividualController.getAllCounterpartyIndividuals);
@@ -12,6 +17,6 @@ router.get('/', counterpartyIndividualController.getAllCounterpartyIndividuals);
 router.get('/:id', counterpartyIndividualController.getCounterpartyIndividualById);
 
 // PUT /api/counterparty-individual/:id
-router.put('/:id', counterpartyIndividualController.updateCounterpartyIndividual);
+router.put('/:id', checkAuth, requireMiddleOffice, counterpartyIndividualController.updateCounterpartyIndividual);
 
 module.exports = router;
