92dee6d130c27e9d844a3be632fb0662ae98abcb
unknown <prashanandradi@gmail.com>
2026-09-08 11:50:14 +0530

    Remove the SQL CDS/custodian joint-relationship migration.

--- migrations/add_cds_custodian_to_joint_relationships.sql
+++
@@ -1,5 +0,0 @@
--- Add CDS Account and Custodian Bank columns to joint_counterparty_relationships table
-ALTER TABLE joint_counterparty_relationships 
-ADD COLUMN IF NOT EXISTS cds_account VARCHAR(255) AFTER mobile,
-ADD COLUMN IF NOT EXISTS custodian_bank VARCHAR(255) AFTER cds_account;
-
