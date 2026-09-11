ebb307a810fab20e4a9030a1dce0858005377d3a
unknown <prashanandradi@gmail.com>
2026-09-11 09:05:27 +0530

    Allow any signed-in user to create or edit joint counterparties.

--- routes/counterpartyJointRoutes.js
+++ routes/counterpartyJointRoutes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const counterpartyJointController = require('../controllers/counterpartyJointController');
 const { checkAuth } = require('../middleware/auth');
-const { requireMiddleOffice } = require('../middleware/requireMiddleOffice');
 
 // Counterparty master setup (create/edit) is Middle Office-exclusive - the
 // dropdown/lookup GET routes below stay open, since every front-office user
 // needs them for deal entry.
 // POST /api/counterparty-joint
-router.post('/', checkAuth, requireMiddleOffice, counterpartyJointController.createCounterpartyJoint);
+router.post('/', checkAuth, counterpartyJointController.createCounterpartyJoint);
 
 // GET /api/counterparty-joint
 router.get('/', counterpartyJointController.getAllCounterpartyJoints);
@@ -17,6 +16,6 @@ router.get('/', counterpartyJointController.getAllCounterpartyJoints);
 router.get('/:id', counterpartyJointController.getCounterpartyJointById);
 
 // PUT /api/counterparty-joint/:id
-router.put('/:id', checkAuth, requireMiddleOffice, counterpartyJointController.updateCounterpartyJoint);
+router.put('/:id', checkAuth, counterpartyJointController.updateCounterpartyJoint);
 
 module.exports = router;
