8a4bddce3e87dfe73e28b1e69e8eb31a52c773f7
unknown <prashanandradi@gmail.com>
2026-09-11 09:05:27 +0530

    Allow any signed-in user to create or edit individual counterparties.

--- routes/counterpartyIndividualRoutes.js
+++ routes/counterpartyIndividualRoutes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const counterpartyIndividualController = require('../controllers/counterpartyIndividualController');
 const { checkAuth } = require('../middleware/auth');
-const { requireMiddleOffice } = require('../middleware/requireMiddleOffice');
 
 // Counterparty master setup (create/edit) is Middle Office-exclusive - the
 // dropdown/lookup GET routes below stay open, since every front-office user
 // needs them for deal entry.
 // POST /api/counterparty-individual
-router.post('/', checkAuth, requireMiddleOffice, counterpartyIndividualController.createCounterpartyIndividual);
+router.post('/', checkAuth, counterpartyIndividualController.createCounterpartyIndividual);
 
 // GET /api/counterparty-individual
 router.get('/', counterpartyIndividualController.getAllCounterpartyIndividuals);
@@ -17,6 +16,6 @@ router.get('/', counterpartyIndividualController.getAllCounterpartyIndividuals);
 router.get('/:id', counterpartyIndividualController.getCounterpartyIndividualById);
 
 // PUT /api/counterparty-individual/:id
-router.put('/:id', checkAuth, requireMiddleOffice, counterpartyIndividualController.updateCounterpartyIndividual);
+router.put('/:id', checkAuth, counterpartyIndividualController.updateCounterpartyIndividual);
 
 module.exports = router;
